4f04546b22b49fa337dd37097e435b03a366048c
nicollycruz300609-dotcom <nicolly.cruz300609@gmail.com>
2026-08-17 15:11:53 -0400

    linha apagada
    Fix typos in date formatting and JSON response.

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ console.log(' data e hora: ${new date().toiSOString()}')
 
 res.statusCode = 201
 res.setHeader('Content-Type', 'application/json');
-res.end("JSON.stringfy({"status":ok})!");
+  //sem o res.end trava o navegador
 });
 
 server.listen(porta, ()=> {
 console.log(`Servidor ouvindo na porta ${porta}`)
 });
+

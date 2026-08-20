8837b9c42f46a687297bd16543527360696f7ad7
nicollycruz300609-dotcom <nicolly.cruz300609@gmail.com>
2026-08-20 17:20:57 +0000

    servidor criado e rota adicionada

--- server.js
+++ server.js
@@ -5,23 +5,38 @@ import { URL } from 'node:url'
 const port = 3000
 
 const status = {
-    "status": "ok",
-    "date": new Date().toISOString()
+"status": "ok",
+"date": new Date().toISOString()
 }
 
 const produtos = [
-    { id: 1, nome: "Sabonete" },
-    { id: 2, nome: "Monitor" },
-    { id: 3, nome: "Cadeira Gamer" }
+{ id: 1, nome: "Sabonete" },
+{ id: 2, nome: "Monitor" },
+{ id: 3, nome: "Cadeira Gamer" }
 ]
 
-res.statusCode = 201
-res.setHeader('Content-Type', 'text/plain; charset=utf-8');
- // o clico nao é finalizado adequadamente (trava a página)
+
+const server = http.createServer((req, res) => {
+const urlObj = new URL(req.url, `http://${req.headers.host}`)
+res.statusCode = 200;
+res.setHeader('Content-Type', 'application/json');
+
+if (req.method == "GET" && urlObj.pathname == "/contato") {
+return res.end(JSON.stringify({
+"numero_telefone": "67 99999-9999",
+"endereco": "Rua da Alegria, 99"
+}));
+}
+
+if (req.method == "GET" && urlObj.pathname == "/produtos") {
+return res.end(JSON.stringify(produtos));
+}
+
+res.end(JSON.stringify({ "data": "Página Inicial" }));
 });
 
 server.listen(port, () => {
-    console.log("Servidor funcionando na porta ", port)
+console.log("Servidor funcionando na porta ", port)
 });
 
 
